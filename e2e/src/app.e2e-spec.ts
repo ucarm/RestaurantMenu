292ad75d1b76b8dac52a1b8fd7c7bb0ce056dd74
ucarm <mehmetucar86@gmail.com>
2019-08-01 01:56:36 -0500

    add a new component

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,8 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to RestaurantMenu!');
+    expect(page.getTitleText()).toEqual('Welcome to Mehmet\'s my RestaurantMenu!');
+    console.log(page.getTitleText());
   });
 
   afterEach(async () => {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
 
 import { AppComponent } from './app.component';
-
 @NgModule({
   declarations: [
     AppComponent

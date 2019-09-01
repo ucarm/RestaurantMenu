9b0b60a21c682c9312a0f712b4f76e7a03a1d776
ucarm <mehmetucar86@gmail.com>
2019-09-01 03:39:37 -0500

    String interpolation/property binding/ event binding/ 2-way databiding work

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -6,10 +6,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
+  allowNewServer = false;
+  serverCreationStatus = "no servers are created";
+  serverName= "";
+  userName="";
 
-  constructor() { }
+  constructor() { 
+    setTimeout(() => {
+      this.allowNewServer =true;
+    }, 2000);
+  }
 
   ngOnInit() {
+  }
+
+  onCreateServer(){
+    this.serverCreationStatus="Server is created. Name is "+this.serverName;
+  }
+
+  onUpdateServerName(event :any){
+    this.serverName= (<HTMLInputElement>event.target).value;
+  }
+
+  resetUsernameField(){
+    this.userName="";
   }
 
 }

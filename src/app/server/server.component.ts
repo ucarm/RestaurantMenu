38a4baf05e9a3abf30b0f2100eb1acfba8ed9231
ucarm <mehmetucar86@gmail.com>
2019-09-02 01:33:20 -0500

    ngFor and ngIf and more practice for property binding, event binding

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -1,15 +1,29 @@
 import { Component } from '@angular/core';
+import { ServersComponent } from '../servers/servers.component';
 
 @Component({
     selector: 'app-server',
     templateUrl: './server.component.html' 
 })
-export class ServerComponent {
-    serverId =10;
+export class ServerComponent{
+    static countServer=0
+    serverId=1;
     serverStatus= "online";
+
+    constructor(){
+        this.serverStatus = Math.random() > 0.5 ? 'online' : 'offline';
+        this.serverId=ServerComponent.countServer+1;
+        ServerComponent.countServer++;
+    }
 
     getServerStatus(){
         return this.serverStatus;
     }
+    getColor(){
+        return this.getServerStatus() === 'online' ? 'green': 'red';
+    }
 
+    getStatusClass(){
+        return  this.getServerStatus() === 'online' ? 'alert alert-success green': 'alert alert-danger';
+    }
 }
--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -8,30 +8,47 @@ import { Component, OnInit } from '@angular/core';
 export class ServersComponent implements OnInit {
   allowNewServer = false;
   serverCreationStatus = "no servers are created";
-  serverName= "";
-  userName="";
-  serverCreated=false;
+  serverName = "";
+  userName = "";
+  serverCreated = false;
+  servers = ['testServer', 'testServer2'];
+  displayDetails=false;
+  displayDetailsClicks = [];
+  displayDetailsClicksCount=0;
 
-  constructor() { 
+  constructor() {
     setTimeout(() => {
-      this.allowNewServer =true;
+      this.allowNewServer = true;
     }, 2000);
   }
 
   ngOnInit() {
   }
 
-  onCreateServer(){
-    this.serverCreated=true;
-    this.serverCreationStatus="Server is created. Name is "+this.serverName;
+  onCreateServer() {
+    this.serverCreated = true;
+    this.servers.push(this.serverName);
+    this.serverCreationStatus = "Server is created. Name is " + this.serverName;
   }
 
-  onUpdateServerName(event :any){
-    this.serverName= (<HTMLInputElement>event.target).value;
+  onUpdateServerName(event: any) {
+    this.serverName = (<HTMLInputElement>event.target).value;
   }
 
-  resetUsernameField(){
-    this.userName="";
+  resetUsernameField() {
+    this.userName = "";
+  }
+  showAMessage() {
+    this.displayDetails= !this.displayDetails;
+    if (this.displayDetails ===true){
+      this.displayDetailsClicksCount++;
+      var today= new Date();
+      var dateTime = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+      this.displayDetailsClicks.push({'value':this.displayDetailsClicksCount, 'DateTime': dateTime});
+    }    
   }
 
+  moreThan4(){
+    return this.displayDetailsClicksCount>4;
+  }
 }
